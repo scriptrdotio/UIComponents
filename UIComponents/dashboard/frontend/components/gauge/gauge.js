21490f1cf9fe62797343657c73789b9a6e88d3db
manollyjohnny <johnny.manolly@elementn.com>
2017-01-31 11:15:36 +0200

    added data fotmatter

--- UIComponents/dashboard/frontend/components/gauge/gauge.js
+++ UIComponents/dashboard/frontend/components/gauge/gauge.js
@@ -43,8 +43,6 @@ angular
                   "label" : "@",
 
                   "labelFontColor" : "@",
-                 
-                  "customSectors" : "<?",
 
                   "startAnimationType" : "@",
 
@@ -118,9 +116,6 @@ angular
 				               if(self.api) {
                                   wsClient.call(self.api, self.apiParams, self.msgTag)
                                    .then(function(data, response) {
-                                       if(typeof self.onFormatData() == "function"){
-                                         data = self.onFormatData()(data);
-                                       }
                                        self.consumeData(data)
                                    });
 				               }
@@ -148,7 +143,10 @@ angular
 	               }
 
 	              this.consumeData = function(data, response) {
-		               this.gaugeValue = data;
+                      if(typeof self.onFormatData() == "function"){
+                        data = self.onFormatData()(data);
+                      }
+		              this.gaugeValue = data;
 	               }
                }
             });
