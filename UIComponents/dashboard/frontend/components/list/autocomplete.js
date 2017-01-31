aee93a55cb73c1eb83cb1ff27520a295f1138a0d
manollyjohnny <johnny.manolly@elementn.com>
2017-01-31 16:13:11 +0200

    Added a data formatter

--- UIComponents/dashboard/frontend/components/list/autocomplete.js
+++ UIComponents/dashboard/frontend/components/list/autocomplete.js
@@ -95,9 +95,6 @@ angular
                if(self.api) {
                  wsClient.call(self.api, self.apiParams, self.msgTag)
                    .then(function(data, response) {
-                   if(typeof self.onFormatData() == "function"){
-                     data = self.onFormatData()(data);
-                   }
                    self.consumeData(data)
                  });
                }
@@ -124,6 +121,9 @@ angular
          }
          
          this.consumeData = function(data, response) {
+           if(typeof self.onFormatData() == "function"){
+             data = self.onFormatData()(data);
+           }
            this.localData = data;
          }
     }
