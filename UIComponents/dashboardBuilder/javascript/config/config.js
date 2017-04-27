1d74fe134dc8e1d415e628e1022743c4a0df6dda
manollyjohnny <johnny.manolly@elementn.com>
2017-04-27 14:46:54 +0300

    updated config file

--- UIComponents/dashboardBuilder/javascript/config/config.js
+++ UIComponents/dashboardBuilder/javascript/config/config.js
@@ -1,7 +1,20 @@
+var anonymousToken = "VzUwRURDNDQ2MQ==";
+
 var login = {
    redirectTarget: "/UIComponents/dashboardBuilder/index.html",
-   expiry:1,
+   expiry:5,
    loginTarget: "/UIComponents/dashboardBuilder/login.html",
    loginTemplateTarget: "/UIComponents/dashboardBuilder/loginTemplate.html",
-   anonymousToken:"VzUwRURDNDQ2MQ=="
+   anonymousToken: anonymousToken
 };
+
+var wssConfig = ["wsClientProvider",function (wsClientProvider) {
+  wsClientProvider.setToken(anonymousToken);
+  wsClientProvider.setPublishChannel("requestChannel");
+  wsClientProvider.setSubscribeChannel("responseChannel");
+}];
+
+var httpsConfig = ["httpClientProvider",function (httpClientProvider) {
+  httpClientProvider.setBaseUrl("https://component.scriptrapps.io");
+  httpClientProvider.setToken(anonymousToken);
+}]
