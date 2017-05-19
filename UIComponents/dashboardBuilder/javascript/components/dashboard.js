ed117355738fb808512e4cb554dc711d15b722bf
manollyjohnny <johnny.manolly@elementn.com>
2017-05-19 17:22:42 +0300

    set read ACL

--- UIComponents/dashboardBuilder/javascript/components/dashboard.js
+++ UIComponents/dashboardBuilder/javascript/components/dashboard.js
@@ -592,7 +592,7 @@ angular
       }
      
       this.setACLs = function(data){
-          this.acls = data.ACL.execute;
+          this.acls = data.ACL.read;
           var array = this.acls.split(";");
           this.users = [];  
           for(var i = 0; i < array.length; i++){
