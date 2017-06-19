cf40618b8c6bbe39d034a8ac7f077002cee1c561
manollyjohnny <johnny.manolly@elementn.com>
2017-06-19 12:44:31 +0300

    Added IFrame Component

---
+++ UIComponents/dashboard/frontend/components/IFrame/IFrame.js
@@ -0,0 +1,29 @@
+angular.module('IFrame', []);
+
+angular
+    .module('IFrame')
+    .component(
+    'scriptrIframe',
+    {
+
+        bindings : {
+            
+            link : "@"
+
+        },
+        templateUrl : '/UIComponents/dashboard/frontend/components/IFrame/IFrame.html',
+        controller : function($scope, $sce, $q, $element, $window, $timeout) {
+
+            var self = this;
+
+            this.$onInit = function() {
+            }
+
+            this.$postLink = function() {
+            }
+            
+            this.trustSrc = function(link){
+              return $sce.trustAsResourceUrl(link);
+            }
+        }
+    });
