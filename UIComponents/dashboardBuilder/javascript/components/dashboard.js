2c584f539a277952acda60d3c20c685f7d91c26b
touta <marvette.feghali@elementn.com>
2017-09-07 17:29:46 +0300

    Fix problem

--- UIComponents/dashboardBuilder/javascript/components/dashboard.js
+++ UIComponents/dashboardBuilder/javascript/components/dashboard.js
@@ -409,7 +409,7 @@ angular
              this.widgets = pluginData.wdg; //This needs fixing
              this.urlParams = pluginData.urlParams;
              this.transport.defaults = pluginData.settings;
-             this.dashboard["widgets"] = this.widgets;
+             this.dashboard["widgets"] = JSON.parse(this.widgets);
          }
        }
        
