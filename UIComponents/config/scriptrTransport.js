fb8c3681f3d534e0611c5490b35856417b1f0290
touta <marvette.feghali@elementn.com>
2018-07-10 14:51:41 +0300

    generic params to be replaced by user configuration when he checks out the project

--- UIComponents/config/scriptrTransport.js
+++ UIComponents/config/scriptrTransport.js
@@ -1,11 +1,11 @@
 var wssConfig = ["wsClientProvider",function (wsClientProvider) {
-  wsClientProvider.setBaseUrl("wss://kitchen-sink.scriptrapps.io");  
-  wsClientProvider.setToken("TzgzNTA4NkQxOA==");
+  wsClientProvider.setBaseUrl("wss://<subdomain>.scriptrapps.io");  
+  wsClientProvider.setToken("<anonymous_token>");
   wsClientProvider.setPublishChannel("requestChannel");
   wsClientProvider.setSubscribeChannel("responseChannel");
 }];
 
 var httpsConfig = ["httpClientProvider",function (httpClientProvider) {
-  httpClientProvider.setBaseUrl("https://kitchen-sink.scriptrapps.io");
-  httpClientProvider.setToken("TzgzNTA4NkQxOA==");
+  httpClientProvider.setBaseUrl("https://<subdomain>.scriptrapps.io");
+  httpClientProvider.setToken("<anonymous_token>");
 }]
