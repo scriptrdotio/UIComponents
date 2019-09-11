41b0d6f2ad9539be710d1f6a576288382889fed4
ammarscriptr <54846175+ammarscriptr@users.noreply.github.com>
2019-09-11 15:13:55 +0300

    add postData to dataService

--- UIComponents/dataService.js
+++ UIComponents/dataService.js
@@ -1,48 +1,101 @@
 angular.module('DataService', []).service(
     "dataService",
-    function(httpClient, wsClient, $location) {
-        
-        this.getData = function(transport, api, apiParams, useWindowParams, msgTag,  consumerFnc, fetchDataInterval, id) {
-              
-             var params = {};
-             if(useWindowParams == "true") {
-                  params = angular.merge($location.search(), apiParams)
-              } else {
-                  params = angular.copy(apiParams);
-              }
-              if (transport == "wss") {
-                  wsClient.onReady.then(function() {
-                    // Subscribe to socket messages with id chart
-                    if(msgTag){
-                        wsClient.subscribe(msgTag, consumerFnc, id);  
-                    }
-                    if(api) {
+    function ($q, httpClient, wsClient, $location) {
+
+        this.postData = function (transport, api, apiParams, useWindowParams, msgTag, consumerFnc, id) {
+            var defer = $q.defer();
+            var params = {};
+            if (useWindowParams == "true") {
+                params = angular.merge($location.search(), apiParams)
+            } else {
+                params = angular.copy(apiParams);
+            }
+            if (api) {
+                //transport wss
+                if (transport == "wss") {
+                    wsClient.onReady.then(function () {
+                        if (msgTag) {
+                            wsClient.subscribe(msgTag, consumerFnc, id);
+                        }
                         wsClient.call(api, params, msgTag)
-                          .then(function(data, response) {
-                          consumerFnc(data)
-                        },
-                        function(err) {
-                          console.log( "reject published promise", err);
-                          consumerFnc();
-                        });
+                            .then(
+                                function (data, response) {
+                                    if (typeof consumerFnc == "function") consumerFnc(data);
+
+                                    defer.resolve({ msg: 'SUCCESS', details: "", data: data });
+                                },
+                                function (err) {
+
+                                    if (typeof consumerFnc == "function") consumerFnc(err);
+                                    defer.resolve({ msg: 'ERROR', details: err, data: {} });
+                                });
+
+                    });
+                } else if (transport == "https") {
+                    httpClient
+                        .post(api, params)
+                        .then(
+                            function (data, response) {
+                                if (typeof consumerFnc == "function") consumerFnc(data)
+                                defer.resolve({ msg: 'SUCCESS', details: "", data: data });
+                            },
+                            function (err) {
+                                if (typeof consumerFnc == "function") consumerFnc(err)
+                                defer.resolve({ msg: 'ERROR', details: err, data: {} });
+
+                            });
+                } else {
+
+                }
+
+            } else {
+                defer.reject({ msg: "ERROR", details: "api is undefined", data: {} });
+            }
+
+            return defer.promise;
+        }
+
+        this.getData = function (transport, api, apiParams, useWindowParams, msgTag, consumerFnc, fetchDataInterval, id) {
+
+            var params = {};
+            if (useWindowParams == "true") {
+                params = angular.merge($location.search(), apiParams)
+            } else {
+                params = angular.copy(apiParams);
+            }
+            if (transport == "wss") {
+                wsClient.onReady.then(function () {
+                    // Subscribe to socket messages with id chart
+                    if (msgTag) {
+                        wsClient.subscribe(msgTag, consumerFnc, id);
+                    }
+                    if (api) {
+                        wsClient.call(api, params, msgTag)
+                            .then(function (data, response) {
+                                consumerFnc(data)
+                            },
+                                function (err) {
+                                    console.log("reject published promise", err);
+                                    consumerFnc();
+                                });
                     }
 
-                  });
-              } else {
+                });
+            } else {
                 if (transport == "https" && api) {
                     httpClient
-                      .get(api, params)
-                      .then(
-                      function(data, response) {
-                        consumerFnc(data)
-                      },
-                      function(err) {
-                        console
-                          .log(
-                          "reject published promise",
-                          err);
-                      });
+                        .get(api, params)
+                        .then(
+                            function (data, response) {
+                                consumerFnc(data)
+                            },
+                            function (err) {
+                                console.log(
+                                    "reject published promise",
+                                    err);
+                            });
                 }
-              }
             }
-    });
+        }
+    }
+);
