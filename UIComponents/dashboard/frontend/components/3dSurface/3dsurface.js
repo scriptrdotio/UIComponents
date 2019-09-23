af7ad3d8a1765bea54f3f40346429c1042e56e6d
ammarscriptr <54846175+ammarscriptr@users.noreply.github.com>
2019-09-23 18:04:43 +0300

    add 3d surface

---
+++ UIComponents/dashboard/frontend/components/3dSurface/3dsurface.js
@@ -0,0 +1,177 @@
+angular.module('D3Surface', ['angularPlotly']);
+
+angular
+    .module('D3Surface')
+    .component(
+        'scriptrThreeDSurface',
+        {
+
+            bindings: {
+
+                "onLoad": "&onLoad",
+
+                "api": "@",
+
+                "data": "<?",
+
+                "layout": "<?",
+
+                "options": "<?",
+
+                "transport": "@",
+
+                "msgTag": "@",
+
+                "apiParams": "<?",
+
+                "onFormatData": "&",
+
+                "fetchDataInterval": "@",
+                //extras
+                "title": "@",
+                "width": "<?",
+                "height": "<?",
+                "xaxis": "@",
+                "yaxis": "@",
+                "zaxis": "@",
+                "barTitle": "@",
+                "barThickness": "<?",
+                "showBar": "@",
+                "colorScale": "<?",
+
+
+            },
+            templateUrl: '/UIComponents/dashboard/frontend/components/plotly/plotly.html',
+            controller: function ($rootScope, $scope, $window, $element, $timeout, httpClient, wsClient, _, $interval, dataService) {
+
+                var self = this;
+
+                self.$onInit = function () {
+                    console.log("on init started",this)
+                    self._apiParams = (self.apiParams) ? angular.copy(self.apiParams) : [];
+                    self.data = self.data ? self.data : [];
+                    self.width = self.width ? self.width : 500;
+                    self.height = self.height ? self.height :500;
+                    self.staticData = angular.copy(self.data);
+                    self.transformedData = angular.copy(self.data);
+                    angular.element($window).on('resize', self.scheduleResize);
+                    self.style={};
+                    self.layout = self.layout ? self.layout :{
+                        title: self.title,
+                        autosize: false,
+                        width: self.width,
+                        height: self.height,
+                        margin: {
+                            l: 65,
+                            r: 50,
+                            b: 65,
+                            t: 90,
+                        },
+                        scene: {
+                            xaxis: { title: self.xaxis },
+                            yaxis: { title: self.yaxis },
+                            zaxis: { title: self.zaxis },
+                        },
+                    };
+                     self.transformedData=[{
+                        showscale:true,
+                        colorscale:self.colorScale,
+                        colorbar: {
+                          title: self.barTitle,
+                          thickness:self.barThickness
+                        },
+                        z: self.staticData.z,
+                        x: self.staticData.x,
+                        y: self.staticData.y,
+                        type: 'surface'
+                      }];
+
+                    
+
+                    self.initDataService(self.transport);
+                }
+
+                self.resize = function () {
+                    console.log("resize called")
+                  
+                           // self.layout.width = $element.parent()[0].clientHeight;
+                            //self.layout.height = $element.parent()[0].clientWidth;
+              
+
+                }
+
+                self.$postLink = function () {
+                    if (self.timeoutId != null) {
+                        $timeout.cancel(self.timeoutId);
+                    }
+                    self.timeoutId = $timeout(self.resize, 300);
+                    $scope.$watch(function ($scope) {
+                        return $scope.$ctrl.data
+                    }, function (newData) {
+                        self.data = newData;
+                    });
+
+                    if (self.data) {
+                        self.timeout = false;
+                        $timeout(function () {
+                            self.consumeData(self.data);
+                        }, 200)
+                    }
+                }
+
+                self.$onDestroy = function () {
+                    angular.element($window).off('resize', self.scheduleResize);
+                    if (self.msgTag) {
+                        wsClient.unsubscribe(self.msgTag, null, $scope.$id);
+                    }
+                    if (self.refreshTimer)
+                        $interval.cancel(self.refreshTimer);
+                }
+
+                self.scheduleResize = function () {
+                    console.log("scheduleResize called")
+                    if (self.timeoutId != null) {
+                        $timeout.cancel(self.timeoutId);
+                    }
+                    return self.timeoutId = $timeout(self.resize, 100);
+                }
+
+                self.initDataService = function (transport) {
+                    console.log("initDataService called")
+                    dataService.getData(transport, self.api, self.apiParams, self.useWindowParams, self.msgTag, self.consumeData.bind(self), self.fetchDataInterval, $scope.$id);
+
+                    if (self.fetchDataInterval && !self.refreshTimer) {
+                        //Assuming this is success
+                        self.refreshTimer = $interval(
+                            function () {
+                                self.initDataService(self.transport)
+                            }, self.fetchDataInterval * 1000);
+                    }
+                }
+
+               self.consumeData = function (data, response) {
+                    console.log("consumeData called", data)
+                    if(data && data.x && data.y && data.z){
+                        self.transformedData=[{
+                        showscale:true,
+                        colorscale:self.colorScale,
+                        colorbar: {
+                          title: self.barTitle,
+                          thickness:self.barThickness
+                        },
+                        z: data.z,
+                        x: data.x,
+                        y: data.y,
+                        type: 'surface'
+                      }];
+                    
+                     console.log("transformedData called", self.transformedData)
+                    }
+
+                    
+
+                }
+
+
+            }
+        });
---
+++ UIComponents/dashboard/frontend/components/3dSurface/angular-plotly.js
@@ -0,0 +1,94 @@
+
+angular.module('angularPlotly', []).directive('angularPlotly', [
+        '$window',
+        function($window, $timeout) {
+            return {
+                restrict: 'E',
+                template: '<div></div>',
+                scope: {
+                    plotlyData: '=',
+                    plotlyLayout: '=',
+                    plotlyOptions: '=',
+                    plotlyEvents: '=',
+                    plotlyManualDataUpdate: '='
+                },
+                link: function(scope, element) {
+                    var graph = element[0].children[0];
+                    var initialized = false;
+
+                    function subscribeToEvents(graph) {
+                      scope.plotlyEvents(graph);
+                    }
+
+                    function onUpdate() {
+                        //No data yet, or clearing out old data
+                        if (!(scope.plotlyData)) {
+                            if (initialized) {
+                                Plotly.Plots.purge(graph);
+                                graph.innerHTML = '';
+                            }
+                            return;
+                        }
+                        
+                            initialized = true;
+                            Plotly.newPlot(graph, scope.plotlyData, scope.plotlyLayout, scope.plotlyOptions);
+                            if (scope.plotlyEvents){
+                              subscribeToEvents(graph);
+                            }
+                        
+                        
+                    }
+
+                    function onResize() {
+                        if (!(initialized && scope.plotlyData)) return; 
+                        var graphDisplay = window.getComputedStyle(graph).display;
+                        if (!graphDisplay || (graphDisplay === "none") || !initialized || !scope.plotlyData) return;
+                        try{
+                            Plotly.Plots.resize(graph);
+                        }catch(e){
+                            
+                        }
+                    }
+                    
+                    scope.$watch(
+                        function(scope) {
+                            return scope.plotlyLayout;
+                        },
+                        function(newValue, oldValue) {
+                            if (angular.equals(newValue, oldValue) && initialized) return;
+                            onUpdate();
+                        }, true);
+
+                    if (!scope.plotlyManualDataUpdate) {
+                        scope.$watch(
+                            function(scope) {
+                            return scope.plotlyData;
+                        },
+                            function(newValue, oldValue) {
+                                if (angular.equals(newValue, oldValue) && initialized) return;
+                                onUpdate();
+                            }, false);
+                    }
+
+                    /**
+                     * Listens to 'tracesUpdated' event broadcasted from controller to update plot.
+                     */
+                    scope.$on('tracesUpdated', function () {
+                        onUpdate();
+                    });
+
+                    scope.$watch(function() {
+                        return {
+                            'h': element[0].offsetHeight,
+                            'w': element[0].offsetWidth
+                        };
+                    }, function(newValue, oldValue) {
+                        if (angular.equals(newValue, oldValue)) return;
+                        onResize();
+                    }, true);
+
+                    angular.element($window).bind('resize', onResize);
+                }
+            };
+        }
+    ]);
