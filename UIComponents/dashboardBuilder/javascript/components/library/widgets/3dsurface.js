9a4ded6a23280317e229fe534bf52fbebbca97fc
ammarscriptr <54846175+ammarscriptr@users.noreply.github.com>
2019-09-24 15:24:29 +0300

    fixes

--- UIComponents/dashboardBuilder/javascript/components/library/widgets/3dsurface.js
+++ UIComponents/dashboardBuilder/javascript/components/library/widgets/3dsurface.js
@@ -1,5 +1,16 @@
+const __onColorScaleChange__=function (modelValue, form, model) {
+    var arr=[];
+    model["color-scale-wrapper"].forEach(function (element) {
+        var inArr=[];
+        inArr[0]=element.priority;
+        inArr[1]=element.color;
+        arr.push(inArr);
+    });
+    model["color-scale"]=arr;
+};
+
 const __3DSURFACE__ = {
-    "name": "3dsurface",
+    "name": "d3surface",
     "label": "3D Surface",
     "class": "scriptr-three-d-surface",
     "commonData": true,
@@ -16,6 +27,7 @@ const __3DSURFACE__ = {
         "show-bar": "true",
         "bar-title": "Pressre",
         "bar-thickness": 20,
+        "color-scale-wrapper": [{priority:0, color:'rgb(0,0,255)'}, {priority:1, color:'rgb(255,0,0)'}],
         "color-scale": [[0, 'rgb(0,0,255)'], [1, 'rgb(255,0,0)']],
     },
     "box": {
@@ -42,8 +54,6 @@ const __3DSURFACE__ = {
                                     "htmlClass": "col-xs-12 col-sm-4",
                                     "items": [
                                         "title",
-                                        "width",
-                                        "height"
 
                                     ]
                                 },
@@ -69,8 +79,48 @@ const __3DSURFACE__ = {
                                     "type": "section",
                                     "htmlClass": "col-xs-12",
                                     "items": [
-                                        "color-scale",
-                                        
+                                        {
+                                            "key": "color-scale-wrapper",
+                                            "title": "Color scale",
+                                            "items": [{
+                                                "type": "section",
+                                                "htmlClass": "row",
+                                                "items": [
+                                                    {
+                                                        "type": "section",
+                                                        "htmlClass": "col-xs-6 ",
+                                                        "items": [{
+                                                            "key": "color-scale-wrapper[].priority",
+                                                            onChange: __onColorScaleChange__,
+                                                        }]
+                                                    },
+
+                                                    {
+                                                        "type": "section",
+                                                        "htmlClass": "col-xs-6 ",
+                                                        "items": [{
+                                                            "key": "color-scale-wrapper[].color",
+                                                            onChange: __onColorScaleChange__,
+                                                            "colorFormat": "hex3",
+                                                            "spectrumOptions": {
+                                                                showInput: true,
+                                                                showAlpha: false,
+                                                                allowEmpty: true,
+                                                                showPalette: true,
+                                                                preferredFormat: 'hex3',
+                                                                palette: [['#fce94f', '#fcaf3e', '#e9b96e'],
+                                                                ['#8ae234', '#729fcf', '#ad7fa8'],
+                                                                ['#ef2929', '#888a85', '#deface']]
+                                                            }
+
+                                                        }]
+                                                    }
+                                                    
+                                                    
+                                                ]
+                                            }]
+                                        }
+
                                     ]
                                 },
                             ]
@@ -143,7 +193,26 @@ const __3DSURFACE__ = {
                 "type": "string",
                 "description": "Array of colors and order, Ex [[0, 'rgb(0,0,255)'], [1, 'rgb(255,0,0)']] . ",
                 "placeholder": "[[0, 'rgb(0,0,255)'], [1, 'rgb(255,0,0)']]"
+            }, "color-scale-wrapper": {
+                "type": "array",
+                "default": [],
+                "items": {
+                    "type": "object",
+                    "properties": {
+                        "priority": {
+                            "title": "Priority",
+                            "type": "number"
+                        },
+                        "color": {
+                            "title": "Color",
+                            "type": "string",
+                            "format": "color",
+                        }
+                    }
+                }
             },
+
+
         },
         "required": []
     }
