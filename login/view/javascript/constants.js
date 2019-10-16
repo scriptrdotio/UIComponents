435ca736be985aba6ab12deb807d71e97a6f7863
ammarscriptr <54846175+ammarscriptr@users.noreply.github.com>
2019-10-16 13:47:59 +0300

    fixes

--- login/view/javascript/constants.js
+++ login/view/javascript/constants.js
@@ -2,7 +2,7 @@ myApp.constant("constants", {
   appTitle: "App",
   login: {
     "background": '', 
-    "logo": '',
+    "logo": '//scriptr-cdn.s3.amazonaws.com/carwash/images/powered-by-scriptr.png',
     "poweredBy": '//scriptr-cdn.s3.amazonaws.com/carwash/images/powered-by-scriptr.png'
   }
 })
