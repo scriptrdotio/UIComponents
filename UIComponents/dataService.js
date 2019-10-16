8fd82de893d772d3fd9634dc336c88a6b65c7f6e
touta <marvette.feghali@elementn.com>
2019-10-16 15:58:01 +0300

    Transforming the service to a provider.

--- UIComponents/dataService.js
+++ UIComponents/dataService.js
@@ -1,100 +1,110 @@
-angular.module('DataService', []).service(
-    "dataService",
-    function ($q, httpClient, wsClient, $location) {
-        
-        
-        
-        this.getData = function (transport, api, apiParams, useWindowParams, msgTag, consumerFnc, fetchDataInterval, id) {
-            var params = {};
-            if(!apiParams){
-                apiParams={};
-            }
-            if (useWindowParams == "true") {
-                params = angular.merge( apiParams,$location.search())
-            } else {
-                params = angular.copy(apiParams);
-            }
-            if (transport == "wss") {
-                wsClient.onReady.then(function () {
-                    // Subscribe to socket messages with id chart
-                    if (msgTag) {
-                        wsClient.subscribe(msgTag, consumerFnc, id);
+angular.module('DataService', [])
+      .provider(
+      		'dataService',
+            function dataServiceProvider() {
+	            var self = this;
+                this._getData = function($q, httpClient, wsClient, $location, transport, api, apiParams, useWindowParams, msgTag, consumerFnc, fetchDataInterval, id) {
+
+                    var params = {};
+                    if(!apiParams){
+                        apiParams={};
                     }
-                    if (api) {
-                        wsClient.call(api, params, msgTag)
-                            .then(function (data, response) {
-                            consumerFnc(data)
-                        },
-                                  function (err) {
-                            console.log("reject published promise", err);
-                            consumerFnc();
-                        });
+                    if (useWindowParams == "true") {
+                        params = angular.merge( apiParams,$location.search())
+                    } else {
+                        params = angular.copy(apiParams);
                     }
-                });
-            } else {
-                if (transport == "https" && api) {
-                    httpClient
-                        .get(api, params)
-                        .then(
-                        function (data, response) {
-                            consumerFnc(data)
-                        },
-                        function (err) {
-                            console.log(
-                                "reject published promise",
-                                err);
+                    if (transport == "wss") {
+                        wsClient.onReady.then(function () {
+                            // Subscribe to socket messages with id chart
+                            if (msgTag) {
+                                wsClient.subscribe(msgTag, consumerFnc, id);
+                            }
+                            if (api) {
+                                wsClient.call(api, params, msgTag)
+                                    .then(function (data, response) {
+                                    consumerFnc(data)
+                                },
+                                          function (err) {
+                                    console.log("reject published promise", err);
+                                    consumerFnc();
+                                });
+                            }
                         });
-                }
-            }
-        }
-        
-        this.postData = function (transport, api, apiParams, useWindowParams, msgTag, consumerFnc, id) {
-            var defer = $q.defer();
-            var params = {};
-            if(!apiParams){
-                apiParams={};
-            }
-            if (useWindowParams == "true") {
-                params = angular.merge( apiParams,$location.search())
-            } else {
-                params = angular.copy(apiParams);
-            }
-            if (api) {
-                //transport wss
-                if (transport == "wss") {
-                    wsClient.onReady.then(function () {
-                        if (msgTag) {
-                            wsClient.subscribe(msgTag, consumerFnc, id);
+                    } else {
+                        if (transport == "https" && api) {
+                            httpClient
+                                .get(api, params)
+                                .then(
+                                function (data, response) {
+                                    consumerFnc(data)
+                                },
+                                function (err) {
+                                    console.log(
+                                        "reject published promise",
+                                        err);
+                                });
                         }
-                        wsClient.call(api, params, msgTag)
-                            .then(
-                            function (data, response) {
-                                if (typeof consumerFnc == "function") consumerFnc(data);
-                                defer.resolve({ msg: 'SUCCESS', details: "", data: data });
-                            },
-                            function (err) {
-                                if (typeof consumerFnc == "function") consumerFnc(err);
-                                defer.resolve({ msg: 'ERROR', details: err, data: {} });
-                            });
-                    });
-                } else if (transport == "https") {
-                    httpClient
-                        .post(api, params)
-                        .then(
-                        function (data, response) {
-                            if (typeof consumerFnc == "function") consumerFnc(data)
-                            defer.resolve({ msg: 'SUCCESS', details: "", data: data });
-                        },
-                        function (err) {
-                            if (typeof consumerFnc == "function") consumerFnc(err)
-                            defer.resolve({ msg: 'ERROR', details: err, data: {} });
-                        });
-                } else {
+                    }
                 }
-            } else {
-                defer.reject({ msg: "ERROR", details: "api is undefined", data: {} });
-            }
-            return defer.promise;
-        }
-    }
-);
+                
+	            this.$get = [
+	                  "$q", "httpClient", "wsClient", "$location",
+	                  function dataServiceFactory($q, httpClient, wsClient, $location) {
+		                  var methods = {
+		                     getData: function (transport, api, apiParams, useWindowParams, msgTag, consumerFnc, fetchDataInterval, id) {
+                                 self._getData($q, httpClient, wsClient, $location, transport, api, apiParams, useWindowParams, msgTag, consumerFnc, fetchDataInterval, id);
+                            },
+
+                            postData: function (transport, api, apiParams, useWindowParams, msgTag, consumerFnc, id) {
+                                var defer = $q.defer();
+                                var params = {};
+                                if(!apiParams){
+                                    apiParams={};
+                                }
+                                if (useWindowParams == "true") {
+                                    params = angular.merge( apiParams,$location.search())
+                                } else {
+                                    params = angular.copy(apiParams);
+                                }
+                                if (api) {
+                                    //transport wss
+                                    if (transport == "wss") {
+                                        wsClient.onReady.then(function () {
+                                            if (msgTag) {
+                                                wsClient.subscribe(msgTag, consumerFnc, id);
+                                            }
+                                            wsClient.call(api, params, msgTag)
+                                                .then(
+                                                function (data, response) {
+                                                    if (typeof consumerFnc == "function") consumerFnc(data);
+                                                    defer.resolve({ msg: 'SUCCESS', details: "", data: data });
+                                                },
+                                                function (err) {
+                                                    if (typeof consumerFnc == "function") consumerFnc(err);
+                                                    defer.resolve({ msg: 'ERROR', details: err, data: {} });
+                                                });
+                                        });
+                                    } else if (transport == "https") {
+                                        httpClient
+                                            .post(api, params)
+                                            .then(
+                                            function (data, response) {
+                                                if (typeof consumerFnc == "function") consumerFnc(data)
+                                                defer.resolve({ msg: 'SUCCESS', details: "", data: data });
+                                            },
+                                            function (err) {
+                                                if (typeof consumerFnc == "function") consumerFnc(err)
+                                                defer.resolve({ msg: 'ERROR', details: err, data: {} });
+                                            });
+                                    } else {
+                                    }
+                                } else {
+                                    defer.reject({ msg: "ERROR", details: "api is undefined", data: {} });
+                                }
+                                return defer.promise;
+                            }
+		                  };
+		                  return methods;
+	                  }];
+            });
