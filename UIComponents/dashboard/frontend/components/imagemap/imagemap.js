008eebe5a087e0c1164698067ca73b864ed9909d
touta <marvette.feghali@elementn.com>
2020-08-24 11:54:49 +0300

    Adding imageMap widget with heatmap layer.

--- UIComponents/dashboard/frontend/components/imagemap/imagemap.js
+++ UIComponents/dashboard/frontend/components/imagemap/imagemap.js
@@ -1,6 +1,6 @@
 angular.module("Imagemap", ['ui-leaflet', 'ComponentsCommon', 'DataService']);
 angular.module('Imagemap').component('scriptrImagemap',{
-	bindings: {
+    bindings: {
         "onLoad" : "&onLoad",   
         "transport": "@",
         "api" : "@",
@@ -11,7 +11,7 @@ angular.module('Imagemap').component('scriptrImagemap',{
         "fetchDataInterval": "@",
         "useWindowParams": "@",
         "serviceTag": "@", //Service Tag is use on the update-data event, as a key to retrieve from the data. If not available all passed data will be consumed
-
+        "heatmap": "<?",
         "data" : "<?",
         "width": "@",
         "height": "@",
@@ -19,34 +19,33 @@ angular.module('Imagemap').component('scriptrImagemap',{
         "maxZoom": "@",
         "imageUrl": "@",
         "markersData": "<?" //object of objects with key and: lat, lng, group(optional), icon(url, unit} 
-        
     },
     templateUrl : '/UIComponents/dashboard/frontend/components/imagemap/imagemap.html',
-    controller : function($scope, httpClient, wsClient, $interval, dataService, $timeout, $window, $element, leafletData, leafletBoundsHelpers) {
+    controller : function($scope, httpClient, wsClient, $interval, dataService, $timeout, $window, $element, leafletData, leafletBoundsHelpers, leafletLayerHelpers, leafletControlHelpers) {
         var self = this;
-        
         self.$onInit = function(){
+            self.transport = (self.transport) ? self.transport : null;
+            self.msgTag = (self.msgTag) ? self.msgTag : null;
+            self.useWindowParams = (self.useWindowParams) ? self.useWindowParams : "true";
+            self.icon = (self.icon)? self.icon : '//scriptr-cdn.s3.amazonaws.com/uicomponents/dashboard-builder/images/imagemap-bg.svg';
             
+            self.heatmap =  (self.heatmap) ? self.heatmap : false;
             self.cw = $element.parent().width();
             self.ch = $element.parent().height();
+            
             var iw = (self.width)? parseInt(self.width) : 500;
             var ih = (self.height)? parseInt(self.height) : 500;
             var maxZoom = Math.ceil( Math.log( (self.cw/iw > self.ch/ih ? iw/self.cw : ih/self.ch) ) / Math.log(2) );
             
             self.minZoom = (self.minZoom)? parseInt(self.minZoom) : 0;
             self.maxZoom = (self.maxZoom)? parseInt(self.maxZoom) : 3;
+            
             self.width = (self.width)? parseInt(self.width) : 500;
             self.height = (self.height)? parseInt(self.height) : 500;
+            
             self.id = "imagemap-"+$scope.$id;
             
-             //timer needs to be one second in order to wait not only for the map to load but also the markers to load
-            setTimeout(function(){
-                leafletData.getMap(self.id).then(function(map) {
-                    console.log('invalidating size');
-                    map.invalidateSize(false);
-                    
-                     if(self.markersData){
-                
+            if(self.markersData){
                 self.markers = {};
                 for(var i = 0; i < self.markersData.length; i++){
                     var theMarker = self.markersData[i];
@@ -54,7 +53,7 @@ angular.module('Imagemap').component('scriptrImagemap',{
                         lat: theMarker.lat, 
                         lng: theMarker.lng,
                         icon: {
-                        	className: 'custom-div-icon',
+                            className: 'custom-div-icon',
                             type: 'div',
                             html: "<div style='background-color:#96c0d0;' class='marker-pin'><div class='marker-content'><span class='indicator-value' style='right: 0px;'>" + ((self.data[theMarker.key])? self.data[theMarker.key] : "?") + "</span></div></div>",
                             iconSize: [90, 90],
@@ -66,7 +65,7 @@ angular.module('Imagemap').component('scriptrImagemap',{
                         tmp["draggable"] =  true;
                     }
                     if(theMarker.group) {
-                         tmp["group"] =  theMarker.group;
+                        tmp["group"] =  theMarker.group;
                     }
                     if(theMarker.icon && theMarker.icon.url) {
                         tmp.icon["iconUrl"] = theMarker.icon.url;
@@ -74,35 +73,30 @@ angular.module('Imagemap').component('scriptrImagemap',{
                     if(theMarker.icon && theMarker.icon.unit) {
                         tmp.icon["unit"] = theMarker.icon.unit;
                     }
-                    
                     if(theMarker.icon && theMarker.icon.unit && theMarker.icon.url ) {
                         tmp.icon.html = "<div style='background-color:#96c0d0;' class='marker-pin'><div class='marker-content'><img width='32px' height='32px' class='markerImg' src='" + theMarker.icon.url + "'/><span class='indicator-value' style='right: 0px;'>" + ((self.data[theMarker.key])? self.data[theMarker.key] : "?") + " " + theMarker.icon.unit + "</span></div></div>"
                     }
-                    
                     if(theMarker.icon && theMarker.icon.unit && !theMarker.icon.url ) {
                         tmp.icon.html = "<div style='background-color:#96c0d0;' class='marker-pin'><div class='marker-content'><span class='indicator-value' style='right: 0px;'>" + ((self.data[theMarker.key])? self.data[theMarker.key] : "?") + " " + theMarker.icon.unit + "</span></div></div>"
                     }
-                    
                     if(theMarker.icon && !theMarker.icon.unit && theMarker.icon.url ) {
                         tmp.icon.html = "<div style='background-color:#96c0d0;' class='marker-pin'><div class='marker-content'><img width='32px' height='32px' class='markerImg' src='" + theMarker.icon.url + "'/><span class='indicator-value' style='right: 0px;'>" + ((self.data[theMarker.key])? self.data[theMarker.key] : "?") + "</span></div></div>"
                     }
-                    
                     self.markers[theMarker.key] = tmp;
                 }
             }
-                    
+            
+            //timer needs to be one second in order to wait not only for the map to load but also the markers to load
+            setTimeout(function(){
+                leafletData.getMap(self.id).then(function(map) {
+                   // map.invalidateSize(false);
                 });
             }, 1000);
-
+            
             $scope.$on('leafletDirectiveMarker.'+self.id+'.dragend', function(event, args){
                 console.log(args.leafletObject._latlng); 
             });
             
-            self.transport = (self.transport) ? self.transport : null;
-            self.msgTag = (self.msgTag) ? self.msgTag : null;
-            self.useWindowParams = (self.useWindowParams) ? self.useWindowParams : "true";
-            
-            self.icon = (self.icon)? self.icon : '//scriptr-cdn.s3.amazonaws.com/uicomponents/dashboard-builder/images/imagemap-bg.svg';
             
             self.defaults = {
                 crs: L.CRS.Simple,
@@ -117,34 +111,50 @@ angular.module('Imagemap').component('scriptrImagemap',{
             self.imageUrl= (self.imageUrl) ? self.imageUrl : '//s3.amazonaws.com/scriptr-cdn/compagno/HVAC-system-7.png';
             
             self.layers = {
-                    baselayers: {
-                        myLayer: {
-                            name: 'My Layer',
-                            type: 'imageOverlay',
-                            url: self.imageUrl,
-                            bounds: [[self.width,0], [0, self.height]],
-                            layerParams: {
-                                showOnSelector: false,
-                                noWrap: true,
-                            }
+                baselayers: {
+                    myLayer: {
+                        name: 'Base Layer',
+                        type: 'imageOverlay',
+                        url: self.imageUrl,
+                        bounds: [[self.height, 0], [0, self.width]],
+                        layerParams: {
+                            showOnSelector: false,
+                            noWrap: true,
                         }
                     }
                 }
-           
-           self.maxBounds = leafletBoundsHelpers.createBoundsFromArray([[self.width, 0], [0, self.height]]);
+            }
+            self.layers.overlays = {};
+            if(self.heatmap) {
+                self.heatLayerInfo = {
+                        name: 'Heat Map',
+                        type: 'heat',
+                        layerOptions: {
+                            minOpacity: 0.05,
+                            maxZoom: 0,
+                            radius: 30,
+                            blur: 15,
+                            max: 1.0,
+                            gradient: {
+                                0.1: 'blue',
+                                0.6: 'lime', 
+                                1: 'red'
+                            }
+                		},
+                        visible: true
+                };
+            }
+            self.maxBounds = leafletBoundsHelpers.createBoundsFromArray([[self.height, 0], [0, self.width]]);
         }
-        
         self.$postLink = function() {
             self.timeoutId = $timeout(self.resize.bind(self), 100);
             angular.element($window).on('resize', self.onResize);
             if ((self.transport == "wss" && (self.api || self.msgTag)) || (self.transport == "https" && self.api)) { //Fetch data from backend
                 initDataService(this.transport);
             } else if (self.data != null) { //set datas info when data binding is changed, this allows the user to change the data through a parent controller
-                
                 $scope.$watch(function($scope) {
                     // wait for the timeout
                     if ($scope.$ctrl.data) {
-                        
                         return $scope.$ctrl.data
                     }
                 }, function(newVal, oldVal) {
@@ -155,8 +165,8 @@ angular.module('Imagemap').component('scriptrImagemap',{
             } else {
                 //Listen on update-data event to build data
                 $scope.$on("update-data", function(event, data) {
-                    if (data == null) { //typeOf data == 'undefined' || data === null
-                        /*if (self.gaugeValue == null) {
+                    if (data == null) { 
+                        /*if (self.markersData == null) {
                             self.noResults = true;
                         }*/
                     } else {
@@ -167,24 +177,20 @@ angular.module('Imagemap').component('scriptrImagemap',{
                         }
                     }
                 });
-
                 $scope.$emit("waiting-for-data");
             }
         }
-        
         self.onResize = function() {
             if (self.timeoutId != null) {
                 $timeout.cancel(self.timeoutId);
             }
             self.timeoutId = $timeout(self.resize.bind(self), 100);
         }
-        
         self.resize = function() {
             self.cw = $element.parent().width();
             self.ch = $element.parent().height();
             self.calculateNotificationsDisplay();
         }
-        
         self.calculateNotificationsDisplay = function() {
             if ($element.parent() && $element.parent().innerWidth() < 240) {
                 self.usePopover = true;
@@ -196,18 +202,14 @@ angular.module('Imagemap').component('scriptrImagemap',{
             if (self.msgTag) {
                 wsClient.unsubscribe(self.msgTag, null, $scope.$id);
             }
-
             if (self.refreshTimer) {
                 $interval.cancel(self.refreshTimer);
             }
-
             if (self.timeoutId != null) {
                 $timeout.cancel(self.timeoutId);
             }
-
             angular.element($window).off('resize', self.onResize);
         }
-        
         var initDataService = function(transport) {
             var requestInfo = {
                 "api": self.api,
@@ -227,7 +229,6 @@ angular.module('Imagemap').component('scriptrImagemap',{
                     }, self.fetchDataInterval * 1000);
             }
         }
-        
         self.consumeData = function(data, response) {
             if (data.status && data.status == "failure") {
                 self.noResults = true;
@@ -241,9 +242,7 @@ angular.module('Imagemap').component('scriptrImagemap',{
                     self.noResults = false;
                     self.stalledData = false;
                     self.data = data;
-                    
                     console.log('consuming data');
-                    
                     if(self.markers){
                         var dataKeys = Object.keys(data);
                         for(var i = 0; i < dataKeys.length; i++){
@@ -252,10 +251,17 @@ angular.module('Imagemap').component('scriptrImagemap',{
                                 self.markers[dataKey].icon.html = "<div style='background-color:#96c0d0;' class='marker-pin'><div class='marker-content'><img width='32px' height='32px' class='markerImg' src='" + self.markers[dataKey].icon.iconUrl + "'/><span class='indicator-value' style='right: 0px;'>" + data[dataKey] + " " + self.markers[dataKey].icon.unit + "</span></div></div>";
                             }
                         }
-                    }else{
-                        console.log('no markers')
+                    }else if (self.heatmap) {
+                     	leafletData.getMap(self.id).then(function(map) {
+                            if(self.heatLayer) {
+                                leafletLayerHelpers.safeRemoveLayer(map, self.heatLayer);
+                            }
+                            self.heatLayerInfo["data"] = data
+                            self.heatLayer = leafletLayerHelpers.createLayer(self.heatLayerInfo)
+                            leafletLayerHelpers.safeAddLayer(map, self.heatLayer);
+                     });
+                       
                     }
-                    
                 } else {
                     self.noResults = true;
                     self.dataFailureMessage = "Failed to update data, invalid data format.";
