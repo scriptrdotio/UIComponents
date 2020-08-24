887f8d5436275c3e5cf4a07b50297a42905427a9
touta <marvette.feghali@elementn.com>
2020-08-24 11:26:48 +0300

    fixing app sample example in layout to use login component under UIComponents

--- UIComponents/layout/frontend/examples/app/layout.js
+++ UIComponents/layout/frontend/examples/app/layout.js
@@ -1,17 +1,10 @@
-var login = {
-   redirectTarget: "/UIComponents/layout/frontend/examples/app/index.html",
-   loginTarget: "/UIComponents/layout/frontend/examples/app/login.html",
-   expiry:1,
-   anonymousToken:"UjkyNTI2OTk1RQ=="
-};
-
 var menuItems = {
   "mainMenu": "col1",
   "col1": [
     {"id":"4", "iconClass":"fa fa-globe", "label": "Users Management", "route":"#/usermanagement", "active":"true"},
     {"id":"5","iconClass":"fa fa-dashboard","label":"Listing","route":"#/load", "active":"false", "sub": "col3"},
     {"id":"2", "iconClass":"fa fa-bell-o", "label": "Notifications", "route": "#/notif","active":"false"},
-    {"id":"3", "iconClass":"fa fa-cogs", "label": "Generic Rule", "route": "#/gr","active":"false"}
+    {"id":"3", "iconClass":"fa fa-cogs", "label": "Generic Rule", "route" : "#/gr","active":"false"}
   ],
   "col3": [
     {"id":"6", "iconClass":"fa fa-dashboard", "label":"Logs", "route":"#/logs", "active":"false"},
@@ -22,9 +15,9 @@ var menuItems = {
 var headerItems = {
   "logo": "https://blog.scriptr.io/wp-content/uploads/2016/10/logo-1.png",
   "items": [
-    {"icon": "fa fa-ticket", "label": "", "route":"#/logout", "class": "pull-left"},
-    {"icon": "fa fa-ticket", "label": "", "route":"#/logout", "class": "pull-left"},
-    {"icon": "fa fa-ticket", "label": "", "route":"#/logout", "class": "pull-left"}
+    {"icon": "fa fa-ticket", "label": "", "route":"#/header1", "class": "pull-left"},
+    {"icon": "fa fa-ticket", "label": "", "route":"#/header2", "class": "pull-left"},
+    {"icon": "fa fa-ticket", "label": "", "route":"#/header3", "class": "pull-left"}
   ],
   "logout": {"route":"#/logout"}
 };
@@ -32,7 +25,7 @@ var headerItems = {
 var routingItems = {
   "params": [
     {"route": "usermanagement", "template": "/UIComponents/layout/frontend/examples/app/groups.html"},
-    {"route": "logout", "template": "/UIComponents/layout/frontend/examples/app/logout.html"}
+    {"route": "logout", "template": "/login/view/logout.html"}
   ],
   "otherwiseOption" : {"template": ""}
 };
